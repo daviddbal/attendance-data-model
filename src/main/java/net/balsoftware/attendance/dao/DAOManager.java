2b25f5787b23f0343fe865ade37f43e21767a6da
David <david@kidreports.com>
2019-06-10 21:16:49 -0600

    got composite build working

--- src/main/java/net/balsoftware/attendance/dao/DAOManager.java
+++ src/main/java/net/balsoftware/attendance/dao/DAOManager.java
@@ -4,6 +4,7 @@ public class DAOManager {
     // Singleton - user getInstance()
     private DAOManager() {
     }
+
     private static class SingletonHelper{
         private static final DAOManager INSTANCE = new DAOManager();
     }
@@ -12,11 +13,19 @@ public class DAOManager {
     }
 
     private EventDAO eventDAO;
+    private StudentDAO studentDAO;
 
     public EventDAO getEventDAO() {
         if (eventDAO == null) {
             eventDAO = new EventDAO();
         }
         return eventDAO;
+    }
+
+    public StudentDAO getStudentDAO() {
+        if (studentDAO == null) {
+            studentDAO = new StudentDAO();
+        }
+        return studentDAO;
     }
 }
---
+++ src/main/java/net/balsoftware/attendance/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package net.balsoftware.attendance.dao;
+
+public class StudentDAO {
+    public String getStudentById(int id) {
+        throw new RuntimeException("not implemented");
+    }
+}
