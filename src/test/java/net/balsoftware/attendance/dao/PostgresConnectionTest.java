bff9d8757792fa2f6eb4c90f31354d53dcda8b1a
David <david@kidreports.com>
2019-06-16 13:31:22 -0600

    cleanup

--- src/test/java/net/balsoftware/attendance/dao/PostgresConnectionTest.java
+++ src/test/java/net/balsoftware/attendance/dao/PostgresConnectionTest.java
@@ -18,38 +18,6 @@ public class PostgresConnectionTest extends TestAbstract {
     public void canGetStudentList() throws SQLException, IOException {
         StudentDAO studentDAO = injector.getInstance(StudentDAO.class);
         List<Student> students = studentDAO.getAllStudents();
-        assertEquals(3, students.size());
-        
-//        String query = "select * from student";
-//        Statement stmt = null;
-//        try {
-//            stmt = connection.createStatement();
-//            ResultSet resultSet = stmt.executeQuery(query);
-//            while (resultSet.next()) {
-//                int id = resultSet.getInt("id");
-//                int createdById = resultSet.getInt("created_by_id");
-//                String createdTimestamp = resultSet.getString("created_timestamp");
-//                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-//                        .appendPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"
-//                        ).parseLenient()
-//                        .appendOffset("+HH:MM", "Z")
-//                        .toFormatter();
-//                ZonedDateTime createdZonedDateTime = ZonedDateTime.parse(createdTimestamp, formatter);
-//                String firstName = resultSet.getString("first_name");
-//                String lastName = resultSet.getString("last_name");
-//
-//                Student student = new Student()
-//                        .withCreatedById(id)
-//                        .withCreatedTimestamp(createdZonedDateTime)
-//                        .withFirstName(firstName)
-//                        .withLastName(lastName)
-//                        .withCreatedById(createdById);
-//                System.out.println(student);
-//            }
-//        } catch (SQLException e ) {
-//            e.printStackTrace();
-//        } finally {
-//            if (stmt != null) { stmt.close(); }
-//        }
+        assertEquals(1, students.size());
     }
 }
